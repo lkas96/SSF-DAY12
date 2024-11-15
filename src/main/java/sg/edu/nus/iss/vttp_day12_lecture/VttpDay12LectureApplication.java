77040537807f13744167bac58eea3995e650c3fd
Lawson Kwek <63045323+lkas96@users.noreply.github.com>
2024-11-15 09:10:53 +0800

    Springboot Template

---
+++ src/main/java/sg/edu/nus/iss/vttp_day12_lecture/VttpDay12LectureApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp_day12_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VttpDay12LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VttpDay12LectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/vttp_day12_lecture/VttpDay12LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp_day12_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VttpDay12LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
